5db45ec76ff40753c3a838cb204f8a8598dbefc9
Kanchan321 <kanchanpoddarmnk1@gmail.com>
2016-10-09 22:38:32 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/artical one', function (req, res) {
+  res.send('artical one is requested');
+});
+app.get('/artical two', function (req, res) {
+  res.send('artical two is requested');
+});
+app.get('/artical three', function (req, res) {
+  res.send('artical three is requested');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
