3525a58ee60710a84c144b211a226fbeb818a967
Kanchan321 <kanchanpoddarmnk1@gmail.com>
2016-10-10 04:56:54 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var element= document.getElementById('main.text');
 element.innerHTML='new value';
 var img= document.getElementById('img');
+img.onclick=funtion(){
+    img.style.marginleft='100px';
+    
+};
